254a23b31d9157ffdbd33807bfb7eebb082d7f1d
Avinash Gupta <avinash12231@gmail.com>
2024-12-13 18:57:48 +0530

    FriendZy

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const express = require("express");
+const {Server} = require("socket.io");
+const {join} = require("node:path");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set("view engine","ejs");
+app.set("views",join(__dirname,"public/views"));
+app.use(express.static(join(__dirname,"public")));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(join(__dirname,"public/views/index.html"));
+});
+
+io.on("connection",(socket)=>{
+    socket.on("chat-message",(msg)=>{
+        io.emit("recv-message",msg);
+    });
+    socket.on("recv-message",(msg)=>{
+        console.log(msg.msg);
+        
+    })
+});
+
+server.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
